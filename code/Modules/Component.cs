1d9a1c3169f74a3e9a41ab7c01c72d387d4fd2ca
Dean Leszman <github@deanleszman.com>
2023-01-13 23:52:49 +0000

    Base component

---
+++ code/Modules/Component.cs
@@ -0,0 +1,38 @@
+﻿using Stasis.AI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stasis.AI.Modules
+{
+    internal class Component
+    {
+        public Component( string name, DamageStatus dmg, PowerStatus pwr ) : this( name )
+        {
+            DamageStatus = dmg;
+            PowerStatus= pwr;
+        }
+
+        public Component( string name ) : this()
+        {
+            Name = name;
+        }
+
+        public Component()
+        {
+            DamageStatus = DamageStatus.None;
+            PowerStatus = PowerStatus.Offline;
+        }
+
+        /// <summary>
+        /// Component name
+        /// </summary>
+        public string Name { get; set; }
+
+        public DamageStatus DamageStatus { get; set; }
+
+        public PowerStatus PowerStatus { get; set; }
+    }
+}
